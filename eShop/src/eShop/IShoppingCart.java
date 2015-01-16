05e9045dfbe487e78a784385cfdcf7e72d9d7dcb
razf <rf6699@gmail.com>
2015-01-16 19:44:42 +0200

    Some more stuff

--- eShop/src/eShop/IShoppingCart.java
+++ eShop/src/eShop/IShoppingCart.java
@@ -1,10 +1,19 @@
 package eShop;
 
+import java.sql.*;
 
 public interface IShoppingCart {
 
 	class CartException extends RuntimeException {}
 	
-	void addToCart(int barcode, int amount) throws CartException;
+	//can either add or update
+	void addToCart(int barcode, int amount);
+	void removeFromCart(int barcode, int amount);
+	//check if best by date hasn't passed.
+	boolean checkIfStillOk(int barcode);
+	
+	int getDiscount(int barcode);
+	
+	
 
 }
